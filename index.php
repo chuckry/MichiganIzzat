d7846ae9d5eeb371e24a6990a54f3f8012ac4acb
Chuckry Vengadam <chuckry@umich.edu>
2015-05-23 13:34:09 -0700

    Templating

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+    	<meta charset="UTF-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href='http://fonts.googleapis.com/css?family=Nixie+One' rel='stylesheet' type='text/css'>
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    	<link rel="stylesheet" type="text/css" href="style.css">
+    	<title>Michigan Izzat</title>
+    </head>
+    <body>
+        <?php include ("header.php") ?>
+    </body>
+    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+   </body>
+</html
