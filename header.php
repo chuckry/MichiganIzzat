ccfee15c70566eaf3aa8cd0a9c0969faccad9696
Chuckry Vengadam <chuckry@umich.edu>
2015-05-23 13:34:50 -0700

    Header template

---
+++ header.php
@@ -0,0 +1,20 @@
+<script src="js/bootstrap.js"></script>
+<div class="navbar navbar-inverse navbar-static-top">
+    <div class="container">
+        <a href="#" class="active navbar-brand">IZZAT</a>
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navHeaderCollapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <div class="collapse navbar-collapse navHeaderCollapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Media</a></li>
+                <li><a href="#">Store</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
