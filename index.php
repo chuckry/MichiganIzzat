108694e98abf10d1bf32d3178488eda45cdd312b
Chuckry Vengadam <cvengadam@lus-cvengadam1.teslamotors.com>
2015-06-05 10:02:18 -0700

    Added background layers to make site more colorfully appealing

--- index.php
+++ index.php
@@ -8,39 +8,41 @@
     <link href='http://fonts.googleapis.com/css?family=Nixie+One' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
-    <script type="text/javascript" src="functions.js"></script>
 	<title>Michigan Izzat</title>
 </head>
-<body onload="switchCollage()">
+<body>
     <?php include ("header.php"); ?>
-    <div class="infoBox">
-        <h2>Who are we?</h2>
-        <p>
-            Michigan Izzat, founded in January of 2013, is the University of Michigan's 
-            premier all-male fusion dance team. As a nationally ranked collegiate team, 
-            we strive to promote dance and culture while staying true to our identity and 
-            university. Both on and off the stage, we affirm this mission through community 
-            service and performances for our campus community as well as the greater 
-            community across the nation. Using high energy choreography, innovative stunts, 
-            and our passion for dance, we come together fusing the styles of hip hop, bhangra 
-            and bollywood to produce a performance intended to inspire, to allure, and to 
-            celebrate Indian as well as American culture.
-        </p>
+    <div id="indexOneLayer" class="backgroundLayer">
+        <div class="infoBox">
+            <h2>Who are we?</h2>
+            <p>
+                Michigan Izzat, founded in January of 2013, is the University of Michigan's 
+                premier all-male fusion dance team. As a nationally ranked collegiate team, 
+                we strive to promote dance and culture while staying true to our identity and 
+                university. Both on and off the stage, we affirm this mission through community 
+                service and performances for our campus community as well as the greater 
+                community across the nation. Using high energy choreography, innovative stunts, 
+                and our passion for dance, we come together fusing the styles of hip hop, bhangra 
+                and bollywood to produce a performance intended to inspire, to allure, and to 
+                celebrate Indian as well as American culture.
+            </p>
+        </div>
     </div>
-    <hr>
-    <div class="infoBox">
-        <h2>FALL TOGETHER, FLY TOGETHER</h2>
-        <div class="imageContainer">
-            <h3>WORK</h3>
-            <img id="WorkImage" src="Images/IzzatWork.jpg" alt="IzzatWorkCollage">
-        </div>
-        <div class="imageContainer">
-            <h3>PLAY</h3>
-            <img id="PlayImage" src="Images/IzzatPlay.jpg" alt="IzzatPlayCollage">
-        </div>
-        <div id="Dance" class="imageContainer">
-            <h3>DANCE</h3>
-            <img src="Images/IzzatAwariJumpNarrow.png" alt="IzzatJump">
+    <div id="indexTwoLayer" class="backgroundLayer">
+        <div class="infoBox">
+            <h2>FALL TOGETHER, FLY TOGETHER</h2>
+            <div class="imageContainer">
+                <h3>WORK</h3>
+                <img id="WorkImage" src="Images/IzzatWork.jpg" alt="IzzatWorkCollage">
+            </div>
+            <div class="imageContainer">
+                <h3>PLAY</h3>
+                <img id="PlayImage" src="Images/IzzatPlay.jpg" alt="IzzatPlayCollage">
+            </div>
+            <div id="Dance" class="imageContainer">
+                <h3>DANCE</h3>
+                <img src="Images/IzzatAwariJumpNarrow.png" alt="IzzatJump">
+            </div>
         </div>
     </div>
     <?php include ("footer.php"); ?>
