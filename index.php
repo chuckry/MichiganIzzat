23d12be90230b2cf1f33bffe4f7ae38d5e80095d
Chuckry Vengadam <chuckry@umich.edu>
2015-06-05 00:12:55 -0700

    Positioning, class names

--- index.php
+++ index.php
@@ -2,44 +2,49 @@
 <?php $currentFile = basename(__FILE__) ?>
 <html lang="en">
 <html>
-    <head>
-    	<meta charset="UTF-8">
-    	<meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href='http://fonts.googleapis.com/css?family=Nixie+One' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    	<link rel="stylesheet" type="text/css" href="style.css">
-    	<title>Michigan Izzat</title>
-    </head>
-    <body>
-        <?php include ("header.php"); ?>
-        <div class="infoBox">
-            <h2>Who are we?</h2>
-            <p>
-                Michigan Izzat, founded in January of 2013, is the University of Michigan's 
-                premier all-male fusion dance team. As a nationally ranked collegiate team, 
-                we strive to promote dance and culture while staying true to our identity and 
-                university. Both on and off the stage, we affirm this mission through community 
-                service and performances for our campus community as well as the greater 
-                community across the nation. Using high energy choreography, innovative stunts, 
-                and our passion for dance, we come together fusing the styles of hip hop, bhangra 
-                and bollywood to produce a performance intended to inspire, to allure, and to 
-                celebrate Indian as well as American culture.
-            </p>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Nixie+One' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+    <script type="text/javascript" src="functions.js"></script>
+	<title>Michigan Izzat</title>
+</head>
+<body onload="switchCollage()">
+    <?php include ("header.php"); ?>
+    <div class="infoBox">
+        <h2>Who are we?</h2>
+        <p>
+            Michigan Izzat, founded in January of 2013, is the University of Michigan's 
+            premier all-male fusion dance team. As a nationally ranked collegiate team, 
+            we strive to promote dance and culture while staying true to our identity and 
+            university. Both on and off the stage, we affirm this mission through community 
+            service and performances for our campus community as well as the greater 
+            community across the nation. Using high energy choreography, innovative stunts, 
+            and our passion for dance, we come together fusing the styles of hip hop, bhangra 
+            and bollywood to produce a performance intended to inspire, to allure, and to 
+            celebrate Indian as well as American culture.
+        </p>
+    </div>
+    <hr>
+    <div class="infoBox">
+        <h2>FALL TOGETHER, FLY TOGETHER</h2>
+        <div class="imageContainer">
+            <h3>WORK</h3>
+            <img id="WorkImage" src="Images/IzzatWork.jpg" alt="IzzatWorkCollage">
         </div>
-        <hr>
-        <div class="infoBox">
-            <h2>Fall Together, Fly Together</h2>
-            <div class="imageContainer">
-                <h3>WORK</h3>
-                <img src="Images/IzzatWork.jpg">
-            </div>
-            <div class="imageContainer">
-                <h3>PLAY</h3>
-                <img src="Images/IzzatPlay.jpg">
-            </div>
+        <div class="imageContainer">
+            <h3>PLAY</h3>
+            <img id="PlayImage" src="Images/IzzatPlay.jpg" alt="IzzatPlayCollage">
         </div>
-        <?php include ("footer.php"); ?>
-    </body>
-    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+        <div id="Dance" class="imageContainer">
+            <h3>DANCE</h3>
+            <img src="Images/IzzatAwariJumpNarrow.png" alt="IzzatJump">
+        </div>
+    </div>
+    <?php include ("footer.php"); ?>
+</body>
+<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </html>
