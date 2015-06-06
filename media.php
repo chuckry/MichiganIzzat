9c6d0b7387346ce124a4c4b2b732a5202eec4ba1
chuckry <chuckry@umich.edu>
2015-06-05 17:43:39 -0700

    Font style

--- media.php
+++ media.php
@@ -2,18 +2,19 @@
 <?php $currentFile = basename(__FILE__) ?>
 <html lang="en">
 <html>
-    <head>
-    	<meta charset="UTF-8">
-    	<meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href='http://fonts.googleapis.com/css?family=Nixie+One' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    	<link rel="stylesheet" type="text/css" href="style.css">
-    	<title>Michigan Izzat</title>
-    </head>
-    <body>
-        <?php include ("header.php"); ?>
-        <?php include ("footer.php"); ?>
-    </body>
-    <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href='http://fonts.googleapis.com/css?family=Nixie+One' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Michigan Izzat</title>
+</head>
+<body>
+    <?php include ("header.php"); ?>
+    <?php include ("footer.php"); ?>
+</body>
+<script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </html>
