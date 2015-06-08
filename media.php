83edaa09d1db1319d92a98de54890b4b9620717b
Chuckry Vengadam <chuckry@umich.edu>
2015-06-07 23:02:18 -0700

    Added embedded links, implemented first section

--- media.php
+++ media.php
@@ -7,12 +7,23 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link href='http://fonts.googleapis.com/css?family=Nixie+One' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
+   	<link href='http://fonts.googleapis.com/css?family=Alegreya+Sans+SC:100,100italic' rel='stylesheet' type='text/css'>    
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>Michigan Izzat</title>
 </head>
 <body>
     <?php include ("header.php"); ?>
+    <div id="Gallery">
+    	<h2>GALLERY</h2>
+    </div>
+    <h2>PERFORMANCES</h2>
+    <div class="backgroundLayer greyBackground">
+	    <div class="videoContainer leftVideo">
+	    	<iframe src="https://www.youtube.com/embed/X1W3eNvPpCM" frameborder="0" allowfullscreen></iframe>
+	    	<p class="rightText">Our performance at Blah blah blah</p>
+	    </div>
+	</div>
     <?php include ("footer.php"); ?>
 </body>
 <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
